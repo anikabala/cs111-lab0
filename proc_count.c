addbf8262870a61ff3a887e5af23c2fd3fb97239
Anika Bala <anikabalak1@gmail.com>
2024-04-11 08:27:12 -0700

    playing with proc/count

--- proc_count.c
+++ proc_count.c
@@ -6,8 +6,17 @@
 
 static struct proc_dir_entry *entry;
 
+//proc_create_single
+// proc_remove
+// for_each_process
+// seq_printf
+
+//Properly create and remove /proc/count when your module loads and unloads, respectively
+// Make /proc/count return some string when you cat /proc/count
+//Make /proc/count return a integer with the number of running processes (or tasks) when you cat /proc/count
+
 static int proc_count(struct seq_file *m, void *v){
-	// TODO: it's all yours :)
+	    //seq_printf(m, "%d\n", 3);
 	return 0;
 }
 
@@ -27,6 +36,6 @@ static void __exit proc_count_exit(void)
 module_init(proc_count_init);
 module_exit(proc_count_exit);
 
-MODULE_AUTHOR("Your Name");
+MODULE_AUTHOR("Anika Balakrishnan");
 MODULE_DESCRIPTION("CS111 lab0 count proc number");
 MODULE_LICENSE("GPL");
